b9ca95db86fcb4fd9c21a2649bf0cde415410b33
chentao <chentao@jscoe.com>
2024-03-12 17:03:17 +0800

    init

---
+++ rtsp-websocket-proxy/src/main/java/org/timothy/rtspwebsocketproxy/RtspWebsocketProxyApplication.java
@@ -0,0 +1,13 @@
+package org.timothy.rtspwebsocketproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspWebsocketProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtspWebsocketProxyApplication.class, args);
+    }
+
+}
---
+++ rtsp-websocket-proxy/src/test/java/org/timothy/rtspwebsocketproxy/RtspWebsocketProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.timothy.rtspwebsocketproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtspWebsocketProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
